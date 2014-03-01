f7d5f74f2e65f2ad1e56ee3667c20c64f2011f46
沈煜 <chrisniael@vip.qq.com>
2014-03-01 21:57:20 +0800

    Update main.php

--- src/main.php
+++ src/main.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * 微信公众平台对接WordPress PHP SDK
+ *
+ * @author     沈煜 <chrisniael@vip.qq.com>
+ * @license    MIT License
+ */
+ 
 // 两种模式中选其一
 require("wechat.php");	//自定义菜单模式
 //require("wechat_normal.php");		//非自定义菜单模式
